d06ef2806debb070863792cec0a89f368df72808
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2023-04-05 07:47:15 +0530

    file

---
+++ Assignment3/ques1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x>y){
+        cout<<"This is largest number "<<x;
+    }
+    else{
+        cout<<"This is largest number "<<y;
+    }
+    return 0;
+}
+// Sample input : a=5 b=7
+//Output = 7 
